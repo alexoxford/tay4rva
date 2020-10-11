e8ce5b42453cb7dc7f6a6d1c6fcd61a1bbc8e5d1
Alex Oxford <alex@oakhaven.org>
2020-10-11 07:39:07 -0400

    extract header

---
+++ htdocs/index.php
@@ -0,0 +1,26 @@
+<html>
+
+    <head>
+        <title>Write-In Taylor Maloney for RVA 5th District</title>
+        <link rel="stylesheet" href="./stylesheet.css">
+        <link rel="icon" type="image/png" href="./images/favicon.png">
+
+    </head>
+
+    <body>
+        <div id="content">
+            <?php include './header.php' ?>
+
+            <h1>Write In Taylor Maloney For 5th District!</h1>
+
+            <h2>Power to the People!</h2>
+
+            <div id="footer">
+                <hr>
+                <a href="www.twitter.com/tay4rva" class="nav-footer">Twitter</a>
+                <a href="www.instagram.com/rvacadre" class="nav-footer">Instagram</a>
+            </div>
+        </div>
+    </body>
+
+</html>
